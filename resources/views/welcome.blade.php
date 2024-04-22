db5410fe49dd11e59f2774c9351f7424712e7f7e
perlaxd365@gmail.com <perlaxd365@mail.com>
2024-04-22 12:07:44 -0500

    avances

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -945,10 +945,8 @@
                                                 <tr>
                                                     <td>
 <map name="">
-    <img src="https://maps.googleapis.com/maps/api/staticmap?center=-9.122424,-78.522164&zoom=13&size=1200x1200&key=AIzaSyBfZGs4MOM0s5Gty1z88S-KcZFYDDcDVic" alt="">
+    <img src="https://maps.googleapis.com/maps/api/staticmap?center=-9.122424,-78.522164&zoom=13&size=1200x1200&key=AIzaSyBfZGs4MOM0s5Gty1z88S-KcZFYDDcDVic&markers=color:red%7Clabel:Facturito%7C-9.122424,-78.522164" alt="">
 </map>
-                                                        
-                                                            <img src="https://maps.googleapis.com/maps/api/staticmap?center=Brooklyn+Bridge,New+York,NY&zoom=13&size=600x300&maptype=roadmap&markers=color:blue%7Clabel:S%7C40.702147,-74.015794&markers=color:green%7Clabel:G%7C40.711614,-74.012318&markers=color:red%7Clabel:C%7C40.718217,-73.998284&key=AIzaSyBfZGs4MOM0s5Gty1z88S-KcZFYDDcDVic">
                                                     </td>
                                                 </tr>
                                             </table>
